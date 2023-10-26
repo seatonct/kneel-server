6fe90a5f5d4a334d4525cffdf7a8bbb939ead221
Charles Seaton <seatonct@gmail.com>
2023-10-26 14:38:17 -0500

    initial commit

---
+++ kneel.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `Metals`
+(
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `metal` NVARCHAR(160) NOT NULL,
+    `price` NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE `Sizes`
+(
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `carets` NUMERIC NOT NULL,
+    `price` NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE `Styles`
+(
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `style` NVARCHAR(160) NOT NULL,
+    `price` NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE `Orders`
+(
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `metal_id` INTEGER NOT NULL,
+    `size_id` INTEGER NOT NULL,
+    `style_id` INTEGER NOT NULL,
+    FOREIGN KEY(`metal_id`) REFERENCES `Metals`(`id`), 
+    FOREIGN KEY(`size_id`) REFERENCES `Sizes`(`id`),
+    FOREIGN KEY(`style_id`) REFERENCES `Styles`(`id`)
+);
+
+ALTER TABLE Orders 
+ADD `timestamp` DATETIME DEFAULT CURRENT_DATE;
