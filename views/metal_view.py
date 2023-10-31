ae9c2b496f43f1d8c7354fd85404b1adbc576c1f
Charles Seaton <seatonct@gmail.com>
2023-10-31 09:34:07 -0500

    completed sizes_view

--- views/metal_view.py
+++ views/metal_view.py
@@ -1,6 +1,6 @@
 import json
 from nss_handler import status
-from repository import db_get_single, db_get_all, db_delete, db_update, db_create
+from repository import db_get_single, db_get_all
 
 
 class MetalsView():
